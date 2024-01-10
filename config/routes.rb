9b783dca8d813e292326129d9d01762b6ed7abfd
Max Kovalev <maxonemore@ya.ru>
2024-01-11 06:27:58 +0700

    #1 added edit page for category

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
-  resources :categories
+  resources :categories, except: :show
 
-  resources :periods do
+  resources :periods, except: :show do
     post :run, on: :collection
     post :stop, on: :collection
   end
-  
 end
