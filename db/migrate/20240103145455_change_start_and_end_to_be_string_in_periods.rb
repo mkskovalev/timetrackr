69aa08211e3ffbf3cfe11dbbcb5df270ba6c1f0d
Max Kovalev <maxonemore@ya.ru>
2024-01-10 16:27:22 +0700

    #11 change start & end type to datetime

--- db/migrate/20240103145455_change_start_and_end_to_be_string_in_periods.rb
+++ db/migrate/20240103145455_change_start_and_end_to_be_string_in_periods.rb
@@ -5,7 +5,7 @@ class ChangeStartAndEndToBeStringInPeriods < ActiveRecord::Migration[7.1]
   end
 
   def down
-    change_column :periods, :start, :datetime
-    change_column :periods, :end, :datetime
+    change_column :periods, :start, 'timestamp using start::timestamp without time zone'
+    change_column :periods, :end, 'timestamp using "end"::timestamp without time zone'
   end
 end
