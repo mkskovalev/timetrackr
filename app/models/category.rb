56d2b96848d32b7002b76dbebbcc46216a2bc5f5
Max Kovalev <maxonemore@ya.ru>
2024-01-16 03:05:26 +0700

    #22 added delete btn to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   belongs_to :parent, class_name: 'Category', optional: true
-  has_many :childrens, class_name: 'Category', foreign_key: 'parent_id'
-  has_many :periods
+  has_many :childrens, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  has_many :periods, dependent: :destroy
 
   validates :name, presence: true
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'associations' do
-    it { should belong_to(:user).dependent(:destroy) }
+    it { should belong_to(:user) }
     it { should belong_to(:parent).class_name('Category').optional }
-    it { should have_many(:periods) }
+    it { should have_many(:childrens).class_name('Category').with_foreign_key('parent_id').dependent(:destroy) }
+    it { should have_many(:periods).dependent(:destroy) }
   end
 
   describe 'validations' do
