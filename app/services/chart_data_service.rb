2df7f1fb403d0a0277b047e430051104a0c91d09
Max Kovalev <maxonemore@ya.ru>
2024-01-16 20:27:48 +0700

    fixed bug with data in chart

--- app/services/chart_data_service.rb
+++ app/services/chart_data_service.rb
@@ -1,7 +1,7 @@
 module ChartDataService
   def self.aggregate_periods_by_days(category, number_of_days = 7)
-    start_date = number_of_days.days.ago.to_date
-    end_date = Date.today
+    start_date = (number_of_days - 1).days.ago.to_date
+    end_date = Date.tomorrow
 
     periods_in_range = category.periods.where(start: start_date..end_date)
 
@@ -12,7 +12,7 @@ module ChartDataService
 
     # Инициализируем хеш с нулевыми значениями для каждого дня в заданном диапазоне
     sorted_periods_by_day = (start_date..end_date).each_with_object({}) do |date, hash|
-      hash[date.strftime("%d.%m")] = periods_by_day[date.strftime("%d.%m")] || 0
+      hash[date.strftime("%d.%m")] = periods_by_day[date.strftime("%d.%m")] || 0 if date != Date.tomorrow
     end
 
     sorted_periods_by_day
