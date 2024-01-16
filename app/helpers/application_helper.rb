b6422d09f2a19f7870d485686446ff6aa7236fa8
Max Kovalev <maxonemore@ya.ru>
2024-01-16 17:17:17 +0700

    #18 added helper for current_page in menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def menu_item(name, path)
+    active_class = current_page?(path) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+
+    content_tag :a, href: path, class: "#{active_class} group flex items-center px-2 py-2 text-sm font-medium rounded-md" do
+    yield if block_given?
+    concat(name)
+    end
+  end
 end
