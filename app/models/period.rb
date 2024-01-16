366c1c0a7d050b0ad41a9f693ffa58f9c6d35ceb
Max Kovalev <maxonemore@ya.ru>
2024-01-16 21:05:00 +0700

    fixed bug with empty period.end

--- app/models/period.rb
+++ app/models/period.rb
@@ -5,6 +5,7 @@ class Period < ApplicationRecord
   validates :start, presence: true
 
   def formatted_time
+    return unless self.end
     total_seconds = (self.end - self.start).to_i
     CategoriesAnalyticsService.seconds_to_time_format(total_seconds)
   end
