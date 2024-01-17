4ef5f50921936225715efc9b0f17cf5e96a15163
Max Kovalev <maxonemore@ya.ru>
2024-01-17 17:02:13 +0700

    #51 added capistrano configs

--- Gemfile
+++ Gemfile
@@ -29,6 +29,13 @@ group :development do
   gem "web-console"
   gem "rack-mini-profiler"
   gem "letter_opener"
+  gem 'capistrano', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-rvm', require: false
+  gem 'capistrano-passenger', require: false
+  gem 'ed25519', '>= 1.2', '< 2.0'
+  gem 'bcrypt_pbkdf', '>= 1.0', '< 2.0'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.18.0"
+
+set :application, "timetrackr"
+set :repo_url, "git@github.com:mkskovalev/timetrackr.git"
+
+set :branch, 'main'
+
+set :deploy_to, "/home/deploy/timetrackr"
+set :deploy_user, 'deploy'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
