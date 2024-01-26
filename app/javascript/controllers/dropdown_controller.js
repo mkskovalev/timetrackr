edaefcb56b9b7160c4cf907e02a827c5fc0460ab
Max Kovalev <maxonemore@ya.ru>
2024-01-26 18:50:40 +0700

    fixed bug with periods table overflow-hidden

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -8,10 +8,27 @@ export default class extends Controller {
     document.addEventListener("click", this.close.bind(this))
   }
   
-  toggleMenu() {
+  toggleMenu(event) {
     this.closeAllMenus()
     this.menuTarget.classList.toggle("hidden")
+    this.positionMenu(event.currentTarget)
   }  
+
+  positionMenu(button) {
+    const buttonRect = button.getBoundingClientRect();
+    const menuHeight = this.menuTarget.offsetHeight;
+    const table = document.getElementById("periods_list");
+    const tableRect = table.getBoundingClientRect();
+    const spaceBelow = tableRect.bottom - buttonRect.bottom;
+  
+    if (spaceBelow < menuHeight) {
+      this.menuTarget.style.bottom = `${buttonRect.height + 6}px`;
+      this.menuTarget.style.top = "auto";
+    } else {
+      this.menuTarget.style.top = `${buttonRect.height + 6}px`;
+      this.menuTarget.style.bottom = "auto";
+    }
+  }
 
   closeAllMenus() {
     this.application.dropdownControllers.forEach(controller => {
