6229b652f4d6f032eb52b544574a1368c9558049
Max Kovalev <maxonemore@ya.ru>
2024-06-08 19:29:12 +0700

    delete sitemap refresh after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,5 +14,3 @@ append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/javascript", "storage"
-
-after 'deploy:published', 'sitemap:refresh:no_ping'
