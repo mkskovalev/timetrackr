24ba9ce64900137617036eb35c79ef40d9c9297b
Max Kovalev <maxonemore@ya.ru>
2024-06-08 18:58:10 +0700

    #194 added sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,8 @@
+SitemapGenerator::Sitemap.default_host = "https://timetrackr.dev"
+SitemapGenerator::Sitemap.sitemaps_path = '/public/'
+
+SitemapGenerator::Sitemap.create do
+  add '/ru'
+  add '/en'
+
+end
