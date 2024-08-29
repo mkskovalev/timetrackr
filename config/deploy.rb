9acdad0b34ae9d37d739b2948b1063e9a9ed2bf5
Max Kovalev <maxonemore@ya.ru>
2024-08-29 11:16:35 +0700

    run convertations when deploying with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,15 @@ append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/javascript", "storage"
+
+namespace :deploy do
+  after :migrating, :run_data_conversion do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'data_conversion:run'
+        end
+      end
+    end
+  end
+end
