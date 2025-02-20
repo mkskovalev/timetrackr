5784592a63c9b33ff1b4d688ee97fc9328e172cf
Max Kovalev <maxonemore@ya.ru>
2025-02-20 12:21:22 +0700

    #213 added report year activity by days

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,4 +2,18 @@ module ReportsHelper
   def report_period_range(report)
     "#{format_utc_datetime(report.start_date)} - #{format_utc_datetime(report.end_date)}"
   end
+
+  def filled_days_count(months_data)
+    months_data.sum { |_, days| days.sum { |_, count| count.to_i } }
+  end
+
+  def day_color_by_status(status, date)
+    if status == 1
+      'violet-600'
+    elsif status == 0 && date <= Time.current.to_date
+      'gray-300'
+    else
+      'gray-100'
+    end
+  end
 end
